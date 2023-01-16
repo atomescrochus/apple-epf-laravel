b34c02fe07135fe242d3e07664a793da387bb4b9
Marc Andrew Landles <malandles@appwapp.com>
2023-01-16 17:38:11 -0500

    Configuration file cleanup + added database connection

--- config/apple-epf.php
+++ config/apple-epf.php
@@ -3,22 +3,54 @@
 return [
 
     /*
-     * You can get those values in the email you should have received from Apple
-     */
-    'user_id' => env('EPF_USER_ID'),
+    |--------------------------------------------------------------------------
+    | Apple's Enterprise Partner Feed (EPF) credentials
+    |--------------------------------------------------------------------------
+    |
+    | You can get those values in the email you should have received from Apple
+    | when joining the performance partner program.
+    |
+    */
+
+    'user_id'  => env('EPF_USER_ID'),
     'password' => env('EPF_PASSWORD'),
 
     /*
-     * Setting it to false will deactivate any artisan command related to EPF. 
-     * Useful to not clutter your artisan CLI when you only want to use the models provided
-     * by the package.
-     */
+    |--------------------------------------------------------------------------
+    | Database connection
+    |--------------------------------------------------------------------------
+    |
+    | Which database connection to use to import. Defaults to 'apple-epf'.
+    |
+    */
+
+    'database_connection' => env('EPF_DATABASE_CONNECTION', 'apple-epf'),
+
+    /*
+    |--------------------------------------------------------------------------
+    | Artisan commands
+    |--------------------------------------------------------------------------
+    |
+    | Setting it to false will deactivate any artisan command related to EPF. 
+    | Useful to not clutter your artisan CLI when you only want to use the models provided
+    | by the package.
+    |
+    */
     
     'include_artisan_cmd' => true,
 
-    'storage_folder' => 'epf-imports',
-    'archive_folder' => 'archives',
-    'extraction_folder' => 'files',
+    /*
+    |--------------------------------------------------------------------------
+    | Storage folders
+    |--------------------------------------------------------------------------
+    |
+    | Folders used when downloading, extracting and processing EPFs.
+    | This package will use the app/local filesystem.
+    |
+    */
 
+    'storage_folder'    => 'epf-imports',
+    'archive_folder'    => 'archives',
+    'extraction_folder' => 'files',
     
 ];
