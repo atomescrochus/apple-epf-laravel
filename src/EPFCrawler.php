cae9581ec441a12f2dd3b6bf15bf3d55b61f2a4c
Marc Andrew Landles <malandles@appwapp.com>
2023-01-17 15:34:21 -0500

    Apple EPF v4+: Fix crawling structure of files

--- src/EPFCrawler.php
+++ src/EPFCrawler.php
@@ -70,23 +70,57 @@ class EPFCrawler
         $this->urlForIncrementalImportFiles = "https://feeds.itunes.apple.com/feeds/epf/v5/current/incremental/current/";
 
         $this->links = collect([
-            'full'        => $this->getFullImportListOfFiles(),
-            'incremental' => $this->getIncrementalImportListOfFiles(),
+            'full'        => collect($this->getFullImportListOfTypes()),
+            'incremental' => collect($this->getIncrementalImportListOfTypes()),
         ]);
 
         $this->currentIndexContent = "";
     }
 
     /**
+     * Gets a full import list per type.
+     * i.e. itunes, match, popularity and pricing
+     *
+     * @return array
+     */
+    private function getFullImportListOfTypes(): array
+    {
+        $this->crawlFolder($this->urlForFullImportFiles);
+
+        $types   = [];
+        $crawler = new Crawler($this->currentIndexContent, $this->urlForFullImportFiles);
+        $links   = collect($crawler->filter('table > tr > td > a')->links());
+        $links   = $links->reject(function ($link) {
+            return str_contains($link->getUri(), 'incremental');
+        });
+
+        // Get the links for each type
+        foreach ($links as $link) {
+            // Get the type name from link by removing the date
+            $type = preg_replace('/[0-9]+|\//', '', $link->getNode()->nodeValue);
+
+            // Get the links for that type
+            $types[$type] = $this->getFullImportListOfFiles($link->getUri());
+        }
+
+        // Get the import time
+        $this->fullImportTime = $this->getDateFromFilename($links->first()->getNode()->nodeValue);
+
+        return $types;
+    }
+
+    /**
      * Gets a full import list of files from the EPF directory.
+     * 
+     * @param  string $url
      *
      * @return \Illuminate\Support\Collection
      */
-    private function getFullImportListOfFiles(): Collection
+    private function getFullImportListOfFiles(string $url): Collection
     {
-        $this->crawlCurrentFolder(false);
+        $this->crawlFolder($url);
 
-        $crawler = new Crawler($this->currentIndexContent, $this->urlForFullImportFiles);
+        $crawler = new Crawler($this->currentIndexContent, $url);
         $links   = collect($crawler->filter('table > tr > td > a')->links());
 
         $links =  $links->reject(function ($link) {
@@ -95,23 +129,51 @@ class EPFCrawler
             return str_contains($link->getUri(), '.md5');
         })->map(function ($link) {
             return $link->getUri();
-        });
-
-        $this->fullImportTime = $this->getDateFromFilename($links->first());
+        });        
 
         return $links;
     }
 
     /**
+     * Gets a incremental import list per type.
+     * i.e. itunes, match, popularity and pricing
+     *
+     * @return array
+     */
+    private function getIncrementalImportListOfTypes(): array
+    {
+        $this->crawlFolder($this->urlForIncrementalImportFiles);
+
+        $types   = [];
+        $crawler = new Crawler($this->currentIndexContent, $this->urlForIncrementalImportFiles);
+        $links   = collect($crawler->filter('table > tr > td > a')->links());
+
+        foreach ($links as $link) {
+            // Get the type name from link by removing the date
+            $type = preg_replace('/[0-9]+|\//', '', $link->getNode()->nodeValue);
+
+            // Get the links for that type
+            $types[$type] = $this->getIncrementalImportListOfFiles($link->getUri());
+        }
+
+        // Get the import time
+        $this->incrementalImportTime = $this->getDateFromFilename($links->first()->getNode()->nodeValue);
+
+        return $types;
+    }
+
+    /**
      * Gets the incremental import list of files from the EPF directory.
+     *
+     * @param  string $url
      *
      * @return \Illuminate\Support\Collection
      */
-    private function getIncrementalImportListOfFiles(): Collection
+    private function getIncrementalImportListOfFiles(string $url): Collection
     {
-        $this->crawlCurrentFolder(true);
+        $this->crawlFolder($url);
 
-        $crawler = new Crawler($this->currentIndexContent, $this->urlForIncrementalImportFiles);
+        $crawler = new Crawler($this->currentIndexContent, $url);
         $links = collect($crawler->filter('table > tr > td > a')->links());
 
         $links = $links->map(function ($link) {
@@ -119,8 +181,6 @@ class EPFCrawler
         })->reject(function ($link) {
             return str_contains($link, '.md5');
         });
-
-        $this->incrementalImportTime = $this->getDateFromFilename($links->first());
 
         return $links;
     }
@@ -140,22 +200,21 @@ class EPFCrawler
     }
 
     /**
-     * Crawl the current folder.
+     * Crawl the folder.
      *
-     * @param bool $incremental
+     * @param  string  $url  The remote url to crawl
      *
      * @return void
      */
-    private function crawlCurrentFolder(bool $incremental = false): void
+    private function crawlFolder(string $url): void
     {
-        $paths             = $this->getEPFFilesPaths();
-        $credentials       = $this->getCredentials();
-        $currentCrawlerUrl = $incremental ? $this->urlForIncrementalImportFiles : $this->urlForFullImportFiles;
+        $paths       = $this->getEPFFilesPaths();
+        $credentials = $this->getCredentials();
 
         Storage::put($paths->get('storage')->epf_folder."/index.html", "");
 
         $client = new Client();
-        $client->request('GET', $currentCrawlerUrl, [
+        $client->request('GET', $url, [
             'auth' => [$credentials->login, $credentials->password],
             'sink' => $paths->get('system')->epf_folder . '/index.html'
         ]);
